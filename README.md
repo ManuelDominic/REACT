a90139fd1a53a3a813bf0975a44258ffbc72b248
Emmanuel Dominic Matembu <emmanuel.matembu@andela.com>
2019-06-30 17:57:44 +0300

    [10001] Initial startup with react javascript

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-[![Build Status](https://travis-ci.org/ManuelDominic/REACT.svg?branch=develop)](https://travis-ci.org/ManuelDominic/REACT) [![Coverage Status](https://coveralls.io/repos/github/ManuelDominic/REACT/badge.svg?branch=develop)](https://coveralls.io/github/ManuelDominic/REACT?branch=develop)
-
 # PERSONAL REACT JAVASCRIPT STARTUP PROJECT
+
+[![Build Status](https://travis-ci.org/ManuelDominic/REACT.svg?branch=develop)](https://travis-ci.org/ManuelDominic/REACT) [![Coverage Status](https://coveralls.io/repos/github/ManuelDominic/REACT/badge.svg?branch=develop)](https://coveralls.io/github/ManuelDominic/REACT?branch=develop) [![Maintainability](https://api.codeclimate.com/v1/badges/88b47fb2532517f1382d/maintainability)](https://codeclimate.com/github/ManuelDominic/REACT/maintainability)
+
+- This is my first tryout/initial startup with react javascript
+- Here i wrote a functional component and Class component
+- I wrote some tests for both components
+
+## To manually test this
+
+- Clone the repo locally on your PC
+- Run npm install to install the required dependencies
+- Run npm start to see the functionality of the app
+- Run npm test to see if the tests are passing
+
+## Author
+
+- Matembu Emmanuel Dominic
--- src/tests/personPost.test.js
+++ src/tests/personPost.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import PersonPost from '../components/personPost';
 import toJson from 'enzyme-to-json';
+import PersonPost from '../components/personPost';
 
 describe('<PersonPost />', () => {
   it('should render correctly in "debug" mode with shallow', () => {
@@ -34,3 +34,77 @@ describe('<PersonPost />', () => {
     component.unmount();
   });
 });
+
+// import React from 'react';
+// import { mount, shallow } from 'enzyme';
+// import PersonPost from '../components/personPost';
+// import toJson from 'enzyme-to-json';
+
+// describe('<PersonPost />', () => {
+// it('onclick button posted name in "debug" mode with shallow', () => {
+//   const component = shallow(<PersonPost debug />);
+//   const button = component.find('button');
+//   expect(component.find('.button').text()).toBe('No!');
+//   button.simulate('click);
+//   button.props().onClick();
+//   expect(toJson(component)).toBe('Yes!');
+// });
+// it('returns No persons with hide props in "debug" mode with shallow', () => {
+//   const component = shallow(<PersonPost debug hide={false} />);
+//   expect(component.find('h3').length).toBe(1);
+//   component.setProps({hide=true});
+//   expect(component.get(0)).toBe('No persons');
+//   // expect(component.get(0)).toBeNull();
+// });
+// it('input on change posted name in "debug" mode with shallow', () => {
+//   const component = shallow(<PersonPost debug />);
+//   const button = component.find('input');
+//   expect(component.find('#myname')).text().toBe('');
+//   component.simulate('change', {currentTarget: {value: 'Manuel'}});
+//   expect(component.find('#myname')).text().toBe('Manuel');
+// });
+// it('update state in "debug" mode with shallow', () => {
+//   const component = shallow(<PersonPost debug />);
+//   expect(component.find('name').length).toBe('');
+//   component.setState({name: 'Manuel'})
+//   expect(component.find('name').length).toBe('Manuel');
+// });
+
+// it('should render posted name in "debug" mode with mount', () => {
+//   const component = mount(<PersonPost debug />);
+//   const button = component.find('submit');
+//   console.log(button.debug());
+//   button.props().onClick();
+//   expect(toJson(component)).toMatchSnapshot();
+// });
+// it('should render posted name in "debug" mode with mount', () => {
+//   const component = mount(<PersonPost debug />);
+//   const button = component.find('submit');
+//   console.log(button.debug());
+//   button.props().onClick();
+//   expect(toJson(component)).toMatchSnapshot();
+// });
+// it('should render posted name in "debug" mode with mount', () => {
+//   const component = mount(<PersonPost debug />);
+//   const button = component.find('submit');
+//   console.log(button.debug());
+//   button.props().onClick();
+//   expect(toJson(component)).toMatchSnapshot();
+// });
+// it('should render posted name in "debug" mode with mount', () => {
+//   const component = mount(<PersonPost debug />);
+//   const button = component.find('submit');
+//   console.log(button.debug());
+//   button.props().onClick();
+//   expect(toJson(component)).toMatchSnapshot();
+// it('handsString timer in "debug" mode with shallow', () => {
+//   const component = shallow(<PersonList debug />);
+//   const returnTrue = PersonList.instance().handleStrings(`Hello Emmauel the time is ${Date().toLocaleTimeString()}`);
+//   expect(returnTrue).toBe(true);
+// });
+// it('handsString timer in "debug" mode with shallow', () => {
+//   const component = shallow(<PersonList debug />);
+//   const returnTrue = PersonList.instance().handleStrings(`Hello Emmauel the time is ${Date().toLocaleTimeString()}`);
+//   expect(returnTrue).toBe(true);
+// });
+// });
